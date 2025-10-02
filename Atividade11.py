027e4ddabd480f1b080f90aca694ad82b55d2e6f
Tufio <wellingtonfrota275@gmail.com>
2025-10-02 14:30:14 -0300

    Atividade11 finalizada

---
+++ Atividade11.py
@@ -0,0 +1,7 @@
+# Crie uma lista com 5 nomes de colegas da turma e exiba o primeiro e o último nome.
+
+nomes=[ 'joâo','lucas','matheus','vitor','amanda' ]
+primeiro_nome= nomes[0]
+ultimo_nome= nomes[4]
+print('primeiro nome:'+primeiro_nome,'ultimo nome:'+ultimo_nome)
+
